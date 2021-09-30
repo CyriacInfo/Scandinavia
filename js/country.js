dea12c47c69f58e05e27ec35f89accf9df456686
cyriacinfo <cyriac.intesse@gmail.com>
2021-09-30 23:04:19 +0200

    Mise à jour de la branche principale de façon barbare

---
+++ js/country.js
@@ -0,0 +1,50 @@
+const country = [
+    {
+        name: "Danemark",
+        travels: "",
+        activity: "",
+        lodging: "",
+        article: [
+          { picture: "https://picsum.photos/200/300", title: "1", paragraph: "lorem", lien: "" },
+          { picture: "https://picsum.photos/200/300", title: "2", paragraph: "lorem", lien: "" },
+          { picture: "https://picsum.photos/200/300", title: "3", paragraph: "lorem", lien: "" },
+        ],
+    },
+    {
+        name: "Suède",
+        travels: "",
+        activity: "",
+        lodging: "",
+        article: [
+          { picture: "https://picsum.photos/200/300", title: "4", paragraph: "lorem", lien: "" },
+          { picture: "https://picsum.photos/200/300", title: "5", paragraph: "lorem", lien: "" },
+          { picture: "https://picsum.photos/200/300", title: "6", paragraph: "lorem", lien: "" },
+        ],
+    },
+    {
+  
+        name: "Norvège",
+        travels: "",
+        activity: "",
+        lodging: "",
+        article: [
+          { picture: "https://picsum.photos/200/300", title: "7", paragraph: "lorem", lien: "" },
+          { picture: "https://picsum.photos/200/300", title: "8", paragraph: "lorem", lien: "" },
+          { picture: "https://picsum.photos/200/300", title: "9", paragraph: "lorem", lien: "" },
+        ],
+    },
+    {
+        name: "Islande",
+        travels: "",
+        activity: "",
+        lodging: "",
+        article: [
+          { picture: "https://picsum.photos/200/300", title: "10", paragraph: "lorem", lien: "" },
+          { picture: "https://picsum.photos/200/300", title: "11", paragraph: "lorem", lien: "" },
+          { picture: "https://picsum.photos/200/300", title: "12", paragraph: "lorem", lien: "" },
+        ],
+      },
+  ];
+
+export default country;
+
