edd2c4c82922b7b2a83bebad017b4c46c5819a5e
cyriacinfo <cyriac.intesse@gmail.com>
2021-09-30 21:52:44 +0200

    Division du JS, Création assets & pages + quelques ligns de CSS

---
+++ js/carousel.js
@@ -0,0 +1,27 @@
+//--------------------------------------------CAROUSEL------------------------------------------------------------
+
+
+let tabIndex = 0;
+function carousel() {
+  
+  //Je vais chercher mes élements ayant la classe slider, c'est a dire mes images (stocker dans un tableau)
+  let slides = document.getElementsByClassName("slider");
+
+  //je crée une boucle pour faire disparaitre mes images
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; // Fait disparaitre l'image relié à l'index[i] notre image grace au diplay none
+  }
+  tabIndex++; // incrementation de l'index
+
+  if (tabIndex > slides.length) {
+    tabIndex = 1;
+  }
+
+  //fait apparaitre mon image derrière celle affiché
+  slides[tabIndex - 1].style.display = "block";
+  setTimeout(carousel, 2000);// réaplique la fonction 2 seconde après être invoqué
+  // 2000 = 2 sec;
+}
+
+//Invocation de la fonction pour qu'elle se lance des que la page est chargée
+carousel();
